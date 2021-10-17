ef47c3d0e97035b5c923955dc4673008d361727e
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2021-10-17 11:01:02 +0100

    add more styling

--- src/KanbanColumn.js
+++ src/KanbanColumn.js
@@ -4,15 +4,23 @@ import "./KanbanColumn.css";
 
 const KanbanColumn = (props) => {
     return (
-        <div className="KanbanColumn">
-            <h2 className="KanbanColumn-name">{props.columnData.name}</h2>
-            <p className="KanbanColumn-numbers">
-                {props.columnData.tasks} / {props.columnData.limit};
-            </p>
-            <div className="KanbanColumns-tasks-container">
-                <KanbanTask />
-                <KanbanTask />
-                <KanbanTask />
+        <div
+            className="KanbanColumn"
+            style={{ backgroundColor: `${props.columnData.color}` }}
+        >
+            <div className="KanbanColumn-main">
+                <h2 className="KanbanColumn-name">{props.columnData.name}</h2>
+                <p className="KanbanColumn-numbers">
+                    {props.columnData.tasks} / {props.columnData.limit};
+                </p>
+                <div className="KanbanColumns-tasks-container">
+                    <KanbanTask />
+                    <KanbanTask />
+                    <KanbanTask />
+                </div>
+            </div>
+            <div className="KanbanColumn-footer">
+                <button className="KanbanColumn-btn-add">Add task</button>
             </div>
         </div>
     );
